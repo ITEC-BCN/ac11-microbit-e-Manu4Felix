b8cb5ba748db8fc4a139639e7a1be11feb0d18d4
Manuel Félix <manuel.felix.7e8@itb.cat>
2025-11-10 22:25:19 +0100

    R6 DONE

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-
+# Gráfico de temperatura en leds (0 a 50 ºC)
+def on_forever():
+    led.plot_bar_graph(max(0, input.temperature()), 50) # 0 el valor mínimo y 50 el máximo
+    serial.write_value("T", input.temperature())
+    basic.pause(200)
+basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,1 +1,7 @@
-
+//  Gráfico de temperatura en leds (0 a 50 ºC)
+basic.forever(function on_forever() {
+    led.plotBarGraph(Math.max(0, input.temperature()), 50)
+    //  0 el valor mínimo y 50 el máximo
+    serial.writeValue("T", input.temperature())
+    basic.pause(200)
+})
